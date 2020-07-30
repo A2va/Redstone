b9c2062bab8a3b597cf75a96eb68d48877a38f57
A2va <49582555+A2va@users.noreply.github.com>
2020-07-30 18:47:07 +0200

    Add template

---
+++ livesearch/js/search.js
@@ -0,0 +1,17 @@
+$("#ls_query").keyup(function(){
+    _this = this;
+    $.each($(".livesearch-table tbody tr"), function() {
+        if($(_this).val().length > 0)
+        {    
+            $(".livesearch-table").show();
+            if($(this).text().toLowerCase().indexOf($(_this).val().toLowerCase()) === -1)
+                $(this).hide();
+            else
+                $(this).show();
+        }
+        else
+        {
+            //$(".livesearch-table").hide();
+        }
+    });
+});
